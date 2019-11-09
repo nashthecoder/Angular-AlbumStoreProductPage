0a68393ca97598973c31ca0968d63959f409a965
NashTheCoder <naijeria@gmail.com>
2019-11-09 23:18:11 +0300

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,11 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule, Routes } from '@angular/router';
 
+
 const appRoutes: Routes = [
   { path: 'products', component: ProductListComponent},
-  { path: 'product/:id', component: ProductPageComponent}
+  { path: 'product/:id', component: ProductPageComponent},
+  { path: '', redirectTo: 'prodcuts', pathMatch: 'full'}
 ];
 
 @NgModule({
